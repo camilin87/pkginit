dc34d208f52ae947eb421f2284adab74162f43f1
Camilo Sanchez <camilin87@gmail.com>
2014-07-24 22:41:51 -0400

    correct long_description read

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ pkg_dir_path = path.dirname(path.realpath(__file__))
 pkg_dir = path.basename(pkg_dir_path)
 
 def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+    fname = path.join(path.dirname(__file__), fname)
+    return open(fname).read()
 
 from distutils.core import setup
 
@@ -13,7 +14,7 @@ setup(
     author = "CASH Productions",
     author_email = "support@cash-productions.com",
     description = ("Generic __index__.py generator"),
-    long_description = open("README.rst").read(),
+    long_description = read("README.rst"),
     license = "GPL v2",
     keywords = "package module loader init",
     url = "https://github.com/camilin87/pkgtools",
